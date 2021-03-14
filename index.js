7a39ed33cf6031fca2107ea3d94351eac322b1f4
naeco <ocean_zyy@yeah.net>
2021-03-14 16:14:56 +0800

    change color opacity

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ window.addEventListener('load', () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgba(${r}, ${g}, ${b}, 0.8)`;
+    return `rgba(${r}, ${g}, ${b}, 1)`;
   }
 });
